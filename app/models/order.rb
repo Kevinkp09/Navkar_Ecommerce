9c183e50e5a2397aa692aedbb0e6a3f28c9d3ac2
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-06-26 16:08:07 +0530

    Implementing COD functionality and razorpay payment integration.

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
   belongs_to :coupon, optional: :true
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
-  enum status: {"Pending": 0, "In Transit": 1, "Out for Delivery": 2, "Delivered": 3}
+  enum status: {"Pending": 0, "In Transit": 1, "Out for Delivery": 2, "Delivered": 3, "Order Submitted": 4}
   enum payment_status: {"pending": 0, "paid": 1}
+  enum payment_method: {"online": 0, "cod": 1}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_112552) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_085132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,6 +137,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_112552) do
     t.string "razorpay_order_id"
     t.string "razorpay_payment_id"
     t.integer "payment_status", default: 0
+    t.integer "payment_method", default: 0
     t.index ["coupon_id"], name: "index_orders_on_coupon_id"
     t.index ["courier_id"], name: "index_orders_on_courier_id"
   end
